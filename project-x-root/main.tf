280c361af70ff4fa204ee37515e2b63944dca507
mshaibek <shaibekovmb@gmail.com>
2024-09-05 13:42:34 -0500

    1st batch of changes for today

--- project-x-root/main.tf
+++ project-x-root/main.tf
@@ -1,30 +1,11 @@
 # Define a list of users
-locals {
-  engineers = [
-    {
-      first_name = "Alina"
-      last_name  = "Stepanov"
-      email      = "alina.ste@edu.312school.com"
-    },
-    {
-      first_name = "Cristian"
-      last_name  = "Butnaru"
-      email      = "cristian.but@edu.312school.com"
-    },
-    {
-      first_name = "William"
-      last_name  = "Pulgarin"
-      email      = "william.pul@edu.312school.com"
-    }
-  ]
-}
 
 # Create modules using for_each
 module "users" {
   source     = "../iam-identity-center-users"
-  for_each   = { for user in local.engineers : "${user.first_name}_${user.last_name}" => user }
+  for_each   = { for user in csvdecode(file("24a_users.csv")) : "${user.firstname}_${user.lastname}" => user }
 
-  first_name = each.value.first_name
-  last_name  = each.value.last_name
+  first_name = each.value.firstname
+  last_name  = each.value.lastname
   email      = each.value.email
 }
