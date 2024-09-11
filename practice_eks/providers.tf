f2f2f418d7dab8c8ba8b37adc6dbcf0eee149f3d
Oleksandrozhoh <oleksandrozhoh@gmail.com>
2024-09-10 20:03:27 -0500

    configured remote backend

--- practice_eks/providers.tf
+++ practice_eks/providers.tf
@@ -5,12 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
-  # backend "s3" {
-  #   bucket = "312-main-account-state-bucket"
-  #   key    = "24a/terraform.tfstate"
-  #   region = "us-east-2"
-  #   dynamodb_table = "24a_state_lock"
-  # }
+  backend "s3" {
+    bucket = "312-main-account-state-bucket"
+    key    = "24a/terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "24a_state_lock"
+  }
 }
 
 # Configure the AWS Provider
