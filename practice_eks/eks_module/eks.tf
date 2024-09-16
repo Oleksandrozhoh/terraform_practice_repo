d2e63f6dd07f56faeee6118e2d02fdec18142b33
Oleksandrozhoh <oleksandrozhoh@gmail.com>
2024-09-16 18:56:47 -0500

    programatic access entry to eks

--- practice_eks/eks_module/eks.tf
+++ practice_eks/eks_module/eks.tf
@@ -6,6 +6,10 @@ resource "aws_eks_cluster" "test_cluster" {
     subnet_ids = var.subnet_ids
   }
 
+    provisioner "local-exec" {
+    command = "aws eks update-cluster-config --name ${self.name} --authenticator-mode API_AND_CONFIG_MAP"
+  }
+
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
   # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
   depends_on = [
