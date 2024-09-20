ce9377e327ef088fe55342d92ab46d57f945eda0
Oleksandrozhoh <oleksandrozhoh@gmail.com>
2024-09-20 11:50:23 -0500

    add security group for rds

--- practice_eks/rds-postgres-module/rds-postgres.tf
+++ practice_eks/rds-postgres-module/rds-postgres.tf
@@ -10,5 +10,22 @@ resource "aws_db_instance" "default" {
   parameter_group_name = "default.postgres16"
   skip_final_snapshot  = true
   manage_master_user_password = true
-  vpc_security_group_ids = ["sg-075bc0d3223262002"]
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_security_group" "rds_sg" {
+  name        = "rds-sg"
+  description = "Allow PostgreSQL traffic to RDS"
+  vpc_id      = data.aws_vpc.default.id  # Use the correct VPC ID
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    security_groups = ["sg-075bc0d3223262002"]  # The EKS security group ID
+  }
 }
